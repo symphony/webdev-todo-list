4e07a1fe97492bffca0105abe3e22965e539a2a9
symphony <jciurc@gmail.com>
2022-07-05 23:53:34 -0600

    set up html file with task form

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuidV4 } from 'uuid';
 
 interface Task {
   id: string;
@@ -8,7 +8,7 @@ interface Task {
 }
 
 const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.querySelector<HTMLInputElement>('new-task-form');
+const form = document.querySelector<HTMLInputElement>('#new-task-form');
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
 const addListItem = (task: Task) => {
@@ -17,14 +17,15 @@ const addListItem = (task: Task) => {
   const checkbox = document.createElement('input');
 
   checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
 };
 
-form?.addEventListener("submit", e => {
+form?.addEventListener('submit', e => {
   e.preventDefault()
-  if (input?.value == "" || input?.value == null) return;
+  if (input?.value == '' || input?.value == null) return;
 
   const newTask: Task = {
     id: uuidV4(),
