f129ee5722f84b2fea24fc1e81367c369928fbaf
symphony <jciurc@gmail.com>
2022-07-06 00:05:45 -0600

    finish todo interface. store tasks in local storage

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { v4 as uuidV4 } from 'uuid';
 
+// types
 interface Task {
   id: string;
   title: string;
@@ -7,21 +8,40 @@ interface Task {
   createdAt: Date;
 }
 
-const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.querySelector<HTMLInputElement>('#new-task-form');
-const input = document.querySelector<HTMLInputElement>('#new-task-title');
+// helpers
+const saveTasks = () => {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+};
+
+const loadTasks = (): Task[] => {
+  const tasks = localStorage.getItem('TASKS');
+  return tasks ? JSON.parse(tasks) : [];
+};
 
 const addListItem = (task: Task) => {
   const item = document.createElement('li');
   const label = document.createElement('label');
   const checkbox = document.createElement('input');
 
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  })
   checkbox.type = 'checkbox';
   checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+  saveTasks();
 };
+
+
+// constants
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector<HTMLInputElement>('#new-task-form');
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
 
 form?.addEventListener('submit', e => {
   e.preventDefault()
@@ -34,5 +54,7 @@ form?.addEventListener('submit', e => {
     createdAt: new Date(),
   };
 
-  addListItem(newTask)
+  tasks.push(newTask);
+  addListItem(newTask);
+  input.value = '';
 });
